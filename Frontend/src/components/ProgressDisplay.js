382bd828e0546ba5aac0b7963278d3500c985713
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-24 18:46:14 +0300

    final

---
+++ Frontend/src/components/ProgressDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProgressDisplay() {
+  return (
+    <div>ProgressDisplay</div>
+  )
+}
---
+++ Frontend/src/components/StatusDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StatusDisplay() {
+  return (
+    <div>StatusDisplay</div>
+  )
+}
