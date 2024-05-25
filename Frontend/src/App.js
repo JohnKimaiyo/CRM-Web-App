b398685248c7a2b25719468fba7cc3a426094c08
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-25 11:14:19 +0300

    final

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+       <CategoriesContext.Provider value={value}>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/ticket" element={<TicketPage />} />
+            <Route path="/ticket/:id" element={<TicketPage editMode={true} />} />
+          </Routes>
+        </BrowserRouter>
+      </CategoriesContext.Provider>
     </div>
   );
 }
