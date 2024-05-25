07d1ad7e1bfa4f4b86d5732d98cad0643c4a660c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-25 12:40:38 +0300

    final

--- Frontend/src/components/Nav.js
+++ Frontend/src/components/Nav.js
@@ -1,10 +1,21 @@
+import logo from '../images/crm logo.png'
+import { useNavigate } from 'react-router-dom'
 import React from 'react'
 
 export default function Nav() {
   return (
+    <Nav>
+
+
+  
     <div className="logo-container">
 
       <img src={logo} alt="logo"/>
     </div>
+     <div className="controls-container">
+     <div className="icon" onClick={() => navigate('/ticket')}>➕</div>
+     <div className="icon" onClick={() => navigate('/')}>❮❮</div>
+   </div>
+   </Nav>
   )
 }
